b5fbeb392fce0f720d3727a599b4739ffeba9e19
Melvin Gruschow and Nicole Sanders <nicole@>
2017-01-10 13:06:26 -0800

    Add base cat vs dog pages and images

--- flashcards/js/scripts.js
+++ flashcards/js/scripts.js
@@ -1,29 +1,35 @@
+// $(document).ready(function(){
+//   $('#1').click(function(){
+//     $('#1p').toggleClass("hidden")
+//   });
+//   $('#2').click(function(){
+//     $('#2p').toggleClass("hidden")
+//   });
+//   $('#3').click(function(){
+//     $('#3p').toggleClass("hidden")
+//   });
+//   $('#4').click(function(){
+//     $('#4p').toggleClass("hidden")
+//   });
+//   $('#5').click(function(){
+//     $('#5p').toggleClass("hidden")
+//   });
+//   $('#6').click(function(){
+//     $('#6p').toggleClass("hidden")
+//   });
+//   $('#7').click(function(){
+//     $('#7p').toggleClass("hidden")
+//   });
+//   $('#8').click(function(){
+//     $('#8p').toggleClass("hidden")
+//   });
+//   $('#9').click(function(){
+//     $('#9p').toggleClass("hidden")
+//   });
+// })
+
 $(document).ready(function(){
-  $('#1').click(function(){
-    $('#1p').toggleClass("hidden")
+  $(this).click(function(){
+    $(this).next("p").toggleClass("hidden");
   });
-  $('#2').click(function(){
-    $('#2p').toggleClass("hidden")
-  });
-  $('#3').click(function(){
-    $('#3p').toggleClass("hidden")
-  });
-  $('#4').click(function(){
-    $('#4p').toggleClass("hidden")
-  });
-  $('#5').click(function(){
-    $('#5p').toggleClass("hidden")
-  });
-  $('#6').click(function(){
-    $('#6p').toggleClass("hidden")
-  });
-  $('#7').click(function(){
-    $('#7p').toggleClass("hidden")
-  });
-  $('#8').click(function(){
-    $('#8p').toggleClass("hidden")
-  });
-  $('#9').click(function(){
-    $('#9p').toggleClass("hidden")
-  });
-})
+});
