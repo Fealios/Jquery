0e2a20b325750d54d1875dc320993929bd798313
Melvin Gruschow and Nicole Sanders <melvin@>
2017-01-10 08:56:18 -0800

    Fix animation slide up and slide down issue with second event handler and add unique ids to spans to correspond with event handlers

--- walrus/js/scripts.js
+++ walrus/js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $('.clickable').click(function(){
+  $('#down').click(function(){
     // $('img').show();
     $('#walrus-showing').slideDown();
     $('#walrus-hidden').slideUp();
   });
-  $('.clickable').click(function(){
+  $('#up').click(function(){
     // $('img').show();
     $('#walrus-showing').slideUp();
     $('#walrus-hidden').slideDown();
