63aea6dca955fec7cdd7cc4341daec17d21af07e
Melvin Gruschow and Nicole Sanders <melvin@>
2017-01-10 09:17:01 -0800

    add 3 images of chocolate and used jquery to hide the images when clicked and show them again by clicking on a paragrah span

---
+++ chocolate/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#chocolate1").click(function() {
+    $("#chocolate1").fadeOut();
+  })
+
+  $("#chocolate2").click(function() {
+    $("#chocolate2").fadeOut();
+  })
+
+  $("#chocolate3").click(function() {
+    $("#chocolate3").fadeOut();
+  })
+
+  $(".show-again").click(function() {
+    $(".fade-back").fadeIn();
+  })
+})
