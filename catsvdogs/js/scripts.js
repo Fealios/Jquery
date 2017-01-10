313540f6a9bd31bde356992c5a5d5c05d42f1bc3
Melvin Gruschow and Nicole Sanders <melvin@>
2017-01-10 14:01:38 -0800

    change the background color to orange if the cat picture is clicked and to green if the dog picture is clicked

--- catsvdogs/js/scripts.js
+++ catsvdogs/js/scripts.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
     $('#thecatsays').append('<li>Meow!</li>');
     $('#thedogsays').append('<li>Bark! </li>');
   });
+
+  $("#cat").click(function(){
+    $("body").removeClass();
+    $("body").addClass("cat-background");
+  })
+
+  $("#dog").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dog-background");
+  })
+
 })
